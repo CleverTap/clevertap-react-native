26f2935d16e4dde55e466e7f928f87e8838aa240
Peter <peter@clevertap.com>
2017-02-16 10:48:37 -0800

    fix gitignore

--- ios/CleverTapReact/CleverTapSDK.framework/Versions/A/Headers/CleverTapUTMDetail.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface CleverTapUTMDetail : NSObject
-@property(nonatomic, strong) NSString *source;
-@property(nonatomic, strong) NSString *medium;
-@property(nonatomic, strong) NSString *campaign;
-@end
