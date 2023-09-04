a9c32b670fec9c36a594d54e7d4b8b0fd316d332
Nishant Kumar <nishant.kumar@clevertap.com>
2023-09-04 16:12:42 +0530

    Adds logic for push impression in sample app in iOS

--- Example/ios/NotificationService/NotificationService.m
+++ Example/ios/NotificationService/NotificationService.m
@@ -1,8 +1,19 @@
 #import "NotificationService.h"
+#import <CleverTap-iOS-SDK/CleverTap.h>
 
 @implementation NotificationService
 
 - (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+  // Add CleverTap Account ID and Account token in your target .plist file
+  [CleverTap setDebugLevel:2];
+  NSDictionary *profile = @{
+                              @"Name": @"testUserA1",
+                              @"Identity": @123456,
+                              @"Email": @"test@test.com"
+  };
+  [[CleverTap sharedInstance] profilePush:profile];
+  [[CleverTap sharedInstance] recordNotificationViewedEventWithData: request.content.userInfo];
+
   [super didReceiveNotificationRequest:request withContentHandler:contentHandler];
 }
 
