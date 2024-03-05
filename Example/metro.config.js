6808baea455d6f1e54f4cff634f80f37128953d9
anush <anush@clevertap.com>
2024-03-05 15:12:24 +0530

    task(SDK-3710) - Upgrades gradle version for example app to 8.2.2.
    - Updates AGP to 8.2.2
    - Updates google-services plugin to 4.4.1
    - Updates react-native to 0.73.5 to support AGP 8
    - Update react-native-reanimated to resolve build issues
    - Adds metro.config.js and svg-transformer to resolve build issues
    - Updates metro.config.js for newer RN version
    - Updates ndk version for react-native-reanimated error
    - Delete ReactNativeFlipper.java as required for latest react-native

--- Example/metro.config.js
+++ Example/metro.config.js
@@ -1,17 +1,31 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+/* Metro configuration for React Native
+* https://github.com/facebook/react-native
+*
+* @format
+*/
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const {
+ resolver: { sourceExts, assetExts },
+} = getDefaultConfig(__dirname);
+
+const config = {
+ transformer: {
+   getTransformOptions: async () => ({
+     transform: {
+       experimentalImportSupport: false,
+       inlineRequires: true,
+     },
+   }),
+   babelTransformerPath: require.resolve('react-native-svg-transformer'),
+ },
+ resolver: {
+   assetExts: assetExts.filter(ext => ext !== 'svg'),
+   sourceExts: [...sourceExts, 'svg'],
+ },
 };
+
+module.exports = mergeConfig(defaultConfig, config);
