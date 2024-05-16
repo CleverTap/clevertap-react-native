33b4f9ac02229fdaa025d3b67ee40bf0ba4357ca
Kushagra Mishra <kushagra@clevertap.com>
2024-05-16 17:42:50 +0530

    Updated Example App with deeplink support

--- Example/ios/Example/AppDelegate.mm
+++ Example/ios/Example/AppDelegate.mm
@@ -1,8 +1,10 @@
 #import "AppDelegate.h"
+#import <UserNotifications/UserNotifications.h>
 
 #import <React/RCTBundleURLProvider.h>
 #import "CleverTap.h"
 #import "CleverTapReactManager.h"
+#import <React/RCTLinkingManager.h>
 
 @implementation AppDelegate
 
@@ -13,9 +15,40 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
   [CleverTap autoIntegrate];
+  [self addNotificationCategories];
   [[CleverTapReactManager sharedInstance] applicationDidLaunchWithOptions:launchOptions];
   
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (void)addNotificationCategories {
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    UNNotificationAction *action1 = [UNNotificationAction actionWithIdentifier:@"action_1" title:@"Back" options:UNNotificationActionOptionNone];
+    UNNotificationAction *action2 = [UNNotificationAction actionWithIdentifier:@"action_2" title:@"Next" options:UNNotificationActionOptionNone];
+    UNNotificationAction *action3 = [UNNotificationAction actionWithIdentifier:@"action_3" title:@"View In App" options:UNNotificationActionOptionNone];
+    UNNotificationCategory *cat = [UNNotificationCategory categoryWithIdentifier:@"CTNotification" actions:@[action1, action2, action3] intentIdentifiers:@[] options:UNNotificationCategoryOptionNone];
+    [center setNotificationCategories:[NSSet setWithObjects:cat, nil]];
+}
+
+// Deep links
+
+- (BOOL)application:(UIApplication *)app
+            openURL:(NSURL *)url
+            options:(NSDictionary *)options {
+  
+  return [RCTLinkingManager application:app openURL:url sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey] annotation:options[UIApplicationOpenURLOptionsAnnotationKey]];
+  
+}
+
+- (void)openURL:(NSURL*)url options:(NSDictionary<NSString *, id> *)options completionHandler:(void (^ __nullable)(BOOL success))completion {
+  
+  completion([RCTLinkingManager application:[UIApplication sharedApplication] openURL:url sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey] annotation:options[UIApplicationOpenURLOptionsAnnotationKey]]);
+  
+}
+
+// Universal links
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
+  return [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
