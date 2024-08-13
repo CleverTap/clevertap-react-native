e7e76f88b5d39ee04cb1366bf4b7e92055fe55c9
anush <anush@clevertap.com>
2024-08-13 15:57:15 +0530

    task(SDK-3792) - Adds support for both old and new arch for the sample app

--- Example/android/app/src/main/java/com/reactnct/MainApplication.java
+++ Example/android/app/src/main/java/com/reactnct/MainApplication.java
@@ -1,46 +1,34 @@
 package com.reactnct;
 
 import android.app.Activity;
+import android.app.Application;
 import android.app.Application.ActivityLifecycleCallbacks;
-import android.app.NotificationManager;
-import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.util.Log;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import com.clevertap.android.pushtemplates.PushTemplateNotificationHandler;
 import com.clevertap.android.sdk.ActivityLifecycleCallback;
 import com.clevertap.android.sdk.CleverTapAPI;
 import com.clevertap.android.sdk.CleverTapAPI.LogLevel;
 import com.clevertap.android.sdk.interfaces.NotificationHandler;
-import com.clevertap.android.sdk.pushnotification.CTPushNotificationListener;
 import com.clevertap.react.CleverTapApplication;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.ReactContext;
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import org.json.JSONObject;
+import android.app.NotificationManager;
+import com.clevertap.android.pushtemplates.PushTemplateNotificationHandler;
+import android.content.Context;
+import androidx.annotation.Nullable;
 
-public class MainApplication extends CleverTapApplication
-        implements ActivityLifecycleCallbacks, ReactApplication{
-
-    private static final String TAG = "MainApplication";
+public class MainApplication extends CleverTapApplication implements ActivityLifecycleCallbacks, ReactApplication {
 
     private final ReactNativeHost mReactNativeHost =
-            new ReactNativeHost(this) {
+            new DefaultReactNativeHost(this) {
                 @Override
                 public boolean getUseDeveloperSupport() {
                     return BuildConfig.DEBUG;
@@ -52,13 +40,23 @@ public class MainApplication extends CleverTapApplication
                     List<ReactPackage> packages = new PackageList(this).getPackages();
                     // Packages that cannot be autolinked yet can be added manually here, for example:
                     // packages.add(new MyReactNativePackage());
-//            packages.add(new CleverTapPackage()); // not to add if done by autolinking
+                    // packages.add(new CleverTapPackage()); // not to add if done by autolinking
                     return packages;
                 }
 
                 @Override
                 protected String getJSMainModuleName() {
                     return "index";
+                }
+
+                @Override
+                protected boolean isNewArchEnabled() {
+                    return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+                }
+
+                @Override
+                protected Boolean isHermesEnabled() {
+                    return BuildConfig.IS_HERMES_ENABLED;
                 }
             };
 
@@ -72,9 +70,13 @@ public class MainApplication extends CleverTapApplication
         CleverTapAPI.setDebugLevel(LogLevel.VERBOSE);
         CleverTapAPI.setNotificationHandler((NotificationHandler) new PushTemplateNotificationHandler());
         CleverTapAPI.getDefaultInstance(getApplicationContext()).enableDeviceNetworkInfoReporting(true);
-        registerActivityLifecycleCallbacks(this);
+        ActivityLifecycleCallback.register(this);
         super.onCreate();
         SoLoader.init(this, /* native exopackage */ false);
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for this app.
+            DefaultNewArchitectureEntryPoint.load();
+        }
         initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     }
 
@@ -94,13 +96,7 @@ public class MainApplication extends CleverTapApplication
                 aClass
                         .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
                         .invoke(null, context, reactInstanceManager);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
+            } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
                 e.printStackTrace();
             }
         }
