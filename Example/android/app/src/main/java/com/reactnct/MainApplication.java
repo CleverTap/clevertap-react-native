8e967c19b50f20c5a18aee0a6f83bbbf1b580cbd
anush <anush@clevertap.com>
2024-10-08 13:32:10 +0530

    Adds a working sample app with RN 0.71 and ct 300

--- Example/android/app/src/main/java/com/reactnct/MainApplication.java
+++ Example/android/app/src/main/java/com/reactnct/MainApplication.java
@@ -5,27 +5,21 @@ import android.app.Application.ActivityLifecycleCallbacks;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
 import com.clevertap.android.pushtemplates.PushTemplateNotificationHandler;
 import com.clevertap.android.sdk.CleverTapAPI;
 import com.clevertap.android.sdk.CleverTapAPI.LogLevel;
 import com.clevertap.react.CleverTapApplication;
+import android.app.Application;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactHost;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactHost;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
-
 import java.util.List;
 
-public class MainApplication extends CleverTapApplication implements ActivityLifecycleCallbacks, ReactApplication {
+public class MainApplication extends CleverTapApplication implements ReactApplication {
 
     private final ReactNativeHost mReactNativeHost =
             new DefaultReactNativeHost(this) {
@@ -40,7 +34,6 @@ public class MainApplication extends CleverTapApplication implements ActivityLif
                     List<ReactPackage> packages = new PackageList(this).getPackages();
                     // Packages that cannot be autolinked yet can be added manually here, for example:
                     // packages.add(new MyReactNativePackage());
-                    // packages.add(new CleverTapPackage()); // not to add if done by autolinking
                     return packages;
                 }
 
@@ -61,66 +54,18 @@ public class MainApplication extends CleverTapApplication implements ActivityLif
             };
 
     @Override
-    public ReactHost getReactHost() {
-        return DefaultReactHost.getDefaultReactHost(getApplicationContext(), mReactNativeHost);
-    }
-
-    @Override
     public ReactNativeHost getReactNativeHost() {
         return mReactNativeHost;
     }
 
     @Override
     public void onCreate() {
-        CleverTapAPI.setDebugLevel(LogLevel.VERBOSE);
-        CleverTapAPI.setNotificationHandler(new PushTemplateNotificationHandler());
-        CleverTapAPI.getDefaultInstance(getApplicationContext()).enableDeviceNetworkInfoReporting(true);
         super.onCreate();
         SoLoader.init(this, /* native exopackage */ false);
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             // If you opted-in for the New Architecture, we load the native entry point for this app.
             DefaultNewArchitectureEntryPoint.load();
         }
-    }
-
-    @Override
-    public void onActivityCreated(@NonNull final Activity activity, @Nullable final Bundle savedInstanceState) {
-
-    }
-
-    @Override
-    public void onActivityStarted(@NonNull final Activity activity) {
-
-    }
-
-    @Override
-    public void onActivityResumed(@NonNull final Activity activity) {
-        Bundle payload = activity.getIntent().getExtras();
-        if (payload != null && payload.containsKey("pt_id") && (payload.getString("pt_id").equals("pt_rating")
-                || payload
-                .getString("pt_id").equals("pt_product_display"))) {
-            NotificationManager nm = (NotificationManager) activity.getSystemService(Context.NOTIFICATION_SERVICE);
-            nm.cancel(payload.getInt("notificationId"));
-        }
-    }
-
-    @Override
-    public void onActivityPaused(@NonNull final Activity activity) {
-
-    }
-
-    @Override
-    public void onActivityStopped(@NonNull final Activity activity) {
-
-    }
-
-    @Override
-    public void onActivitySaveInstanceState(@NonNull final Activity activity, @NonNull final Bundle outState) {
-
-    }
-
-    @Override
-    public void onActivityDestroyed(@NonNull final Activity activity) {
-
+//        ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     }
 }
