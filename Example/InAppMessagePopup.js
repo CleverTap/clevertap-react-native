0d85c04e0618177cab44640c16d9a5128869b511
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2024-10-09 17:11:39 +0300

    Reuse styles

--- Example/InAppMessagePopup.js
+++ Example/InAppMessagePopup.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Modal, View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
+const styles = require('./styles');
+
 const InAppMessagePopup = ({ visible, title, description, isFunction,
    onCancel, onConfirm, onTriggerAction, onFileOpen }) => {
   const [actionName, setActionName] = useState('');
@@ -26,7 +28,7 @@ const InAppMessagePopup = ({ visible, title, description, isFunction,
             onChangeText={setFileName}
           />
           <TouchableOpacity style={styles.submitButton} onPress={() => onFileOpen(fileName)}>
-            <Text style={styles.submitButtonText}>Open File</Text>
+            <Text style={styles.buttonText}>Open File</Text>
           </TouchableOpacity>
 
           {!isFunction &&
@@ -40,7 +42,7 @@ const InAppMessagePopup = ({ visible, title, description, isFunction,
                 onChangeText={setActionName}
               />
               <TouchableOpacity style={styles.submitButton} onPress={() => onTriggerAction(actionName)}>
-                <Text style={styles.submitButtonText}>Trigger Action</Text>
+                <Text style={styles.buttonText}>Trigger Action</Text>
               </TouchableOpacity>
             </View>
           }
@@ -58,78 +60,5 @@ const InAppMessagePopup = ({ visible, title, description, isFunction,
     </Modal>
   );
 };
-
-const styles = StyleSheet.create({
-  modalBackground: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  popupContainer: {
-    width: '80%',
-    padding: 20,
-    backgroundColor: 'white',
-    borderRadius: 10,
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  description: {
-    fontSize: 14,
-    color: 'gray',
-    marginBottom: 20,
-    textAlign: 'center',
-  },
-  inputContainer: {
-    width: '100%'
-  },
-  input: {
-    width: '100%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginBottom: 20
-  },
-  submitButton: {
-    width: '100%',
-    padding: 10,
-    backgroundColor: '#4CAF50',
-    borderRadius: 5,
-    marginBottom: 20,
-  },
-  submitButtonText: {
-    color: 'white',
-    textAlign: 'center',
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-  },
-  cancelButton: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: '#f44336',
-    borderRadius: 5,
-    marginRight: 10,
-  },
-  confirmButton: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: '#4CAF50',
-    borderRadius: 5,
-    marginLeft: 10,
-  },
-  buttonText: {
-    color: 'white',
-    textAlign: 'center',
-  },
-});
 
 export default InAppMessagePopup;
