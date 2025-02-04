53cc4b69ca20e6d81b9579f72b473ae863619f52
anush <anush@clevertap.com>
2025-02-04 16:53:57 +0530

    task(SDK-4349) - Sample app changes for multi-triggers eventlog

--- Example/app/app-utils.js
+++ Example/app/app-utils.js
@@ -121,36 +121,36 @@ export const pushChargedEvent = () => {
 
 export const getUserEventLog = () => {
     CleverTap.getUserEventLog("testEvent", (err, res) => {
-        console.log('User Event Log: ', res, err);
-        showToast(`User Event Log: \n ${JSON.stringify(res)}`);
+        console.log('User Event Log: ', res);
+        showToast(`User Event Log: ${JSON.stringify(res)}`);
     });
 };
 
 export const getUserEventLogCount = () => {
     CleverTap.getUserEventLogCount("testEvent", (err, res) => {
-        console.log('User Event Log Count: ', res, err);
-        showToast(`User Event Log Count: \n ${res}`);
+        console.log('User Event Log Count: ', res);
+        showToast(`User Event Log Count: ${res}`);
     });
 };
 
 export const getUserLastVisitTs = () => {
     CleverTap.getUserLastVisitTs((err, res) => {
-        console.log('User Last Visit Timestamp: ', res, err);
-        showToast(`User Last Visit Timestamp: \n ${res}`);
+        console.log('User Last Visit Timestamp: ', res);
+        showToast(`User Last Visit Timestamp: ${res}`);
     });
 };
 
 export const getUserAppLaunchCount = () => {
     CleverTap.getUserAppLaunchCount((err, res) => {
-        console.log('User App Launch Count: ', res, err);
-        showToast(`User App Launch Count: \n ${res}`);
+        console.log('User App Launch Count: ', res);
+        showToast(`User App Launch Count: ${res}`);
     });
 };
 
 export const getUserEventLogHistory = () => {
     CleverTap.getUserEventLogHistory((err, res) => {
-        console.log('User Event Log History: ', res, err);
-        showToast(`User Event Log History: \n ${JSON.stringify(res)}`);
+        console.log('User Event Log History: ', res);
+        showToast(`User Event Log History: ${JSON.stringify(res)}`);
     });
 };
 
